d78e988bc2d37a581ed926890e2d9009d894a734
Kentara <9081603360ken@gmail.com>
2023-03-28 16:26:36 +0300

    action

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,15 @@
 fun main() {
 
-    println(commission("MasterCart", 14000, 75_001))
-    println(commission("Mir", 1000))
-    println(commission("VK Play", 15500))
+    println(commission("MasterCart", 2000, 1000, "*"))
+    println(commission("Mir", 10000))
+    println(commission("VK Play", 15, 41424))
 
 }
-fun commission(name: String = "VK Play", transferAmount: Int, transferAmountMonth:Int = 0): Int {
 
+fun commission(name: String = "VK Play", transferAmount: Int, transferAmountMonth: Int = 0, action: String = "#"): Int {
     when (name) {
         "MasterCart", "Maestro", "Visa", "Mir" -> if (maximumTransferPer(name, transferAmount, transferAmountMonth)) {
-            return commissionAmount(name, transferAmount, transferAmountMonth)
+            return commissionAmount(name, transferAmount, transferAmountMonth, action)
         }
 
         "VK Play" -> if (maximumTransferPer(name, transferAmount, transferAmountMonth)) {
@@ -18,10 +18,10 @@ fun commission(name: String = "VK Play", transferAmount: Int, transferAmountMont
     }
     return -1
 }
+
 fun maximumTransferPer(name: String, transferAmount: Int, transferAmountMonth: Int): Boolean {
 
     when (name) {
-
         "MasterCart", "Maestro", "Visa", "Mir" -> if (transferAmountMonth < 600_000 || transferAmount < 150_000) {
             return true
         }
@@ -33,23 +33,26 @@ fun maximumTransferPer(name: String, transferAmount: Int, transferAmountMonth: I
     return false
 }
 
-fun commissionAmount(name: String, transferAmount: Int, transferAmountMonth: Int): Int {
+fun commissionAmount(name: String, transferAmount: Int, transferAmountMonth: Int, action: String): Int {
     when (name) {
-        "MasterCart", "Maestro" -> return when (transferAmountMonth) {
-            in 300..75_000 -> {
-                0
+        "MasterCart", "Maestro" -> if (action(action)) {
+            when (transferAmountMonth) {
+                in 300..75_000 -> return 0
             }
+        } else return ((transferAmount * 0.6 / 100) + 20).toInt()
 
-            else -> {
-                ((transferAmount * 0.6 / 100) + 20).toInt()
-            }
-        }
-
-        "Visa", "Mir" -> if (35 < ((transferAmount * 100 / 0.75) - transferAmount)) {
-
-            return 35
-        } else (transferAmount * 100 / 0.75).toInt()
-
+        "Visa", "Mir" -> return if (35 < (transferAmount / 100 * 0.75)) {
+            (transferAmount / 100 * 0.75).toInt()
+        } else 35
     }
-    return transferAmount
+    return 0
+}
+
+fun action(name: String, action: String = "#"): Boolean {
+    when (name) {
+        "MasterCart", "Maestro" -> if (action === "*") {
+            return true
+        }
+    }
+    return false
 }

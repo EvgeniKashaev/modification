e3447fcc423a0fdaaa25e41bbdab24e6f586b6ff
Kentara <9081603360ken@gmail.com>
2023-03-28 13:17:22 +0300

    modification

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,38 +6,27 @@ fun main() {
 
 }
 
-fun commission(name: String, transferAmount: Int): Int {
+fun commission(name: String = "MasterCart", transferAmount: Int, transferAmountMonth:Int = 0): Int {
     when (name) {
-        "MasterCart" -> if (maximumTransferPer(name, transferAmount)) {
+        "MasterCart", "Mir" -> if (maximumTransferPer(name, transferAmount, transferAmountMonth)) {
             return commissionAmount(name, transferAmount)
         }
 
-        "VkPlay" -> if (maximumTransferPer(name, transferAmount)) {
+        "VkPlay" -> if (maximumTransferPer(name, transferAmount, transferAmountMonth)) {
             return transferAmount
-        }
-
-        "Mir" -> if (maximumTransferPer(name, transferAmount)) {
-            return commissionAmount(name, transferAmount)
         }
     }
     return -1
 }
+fun maximumTransferPer(name: String, transferAmount: Int, transferAmountMonth: Int): Boolean {
 
-var transferAmountMonth = 0
-
-fun maximumTransferPer(name: String, transferAmount: Int): Boolean {
-    transferAmountMonth += transferAmount
     when (name) {
 
-        "MasterCart" -> if (transferAmountMonth < 600_000 || transferAmount < 150_000) {
+        "MasterCart", "Mir" -> if (transferAmountMonth < 600_000 || transferAmount < 150_000) {
             return true
         }
 
         "VkPlay" -> if (transferAmountMonth < 40_000 || transferAmount < 15_000) {
-            return true
-        }
-
-        "Mir" -> if (transferAmountMonth < 600_000 || transferAmount < 150_000) {
             return true
         }
     }
@@ -50,15 +39,14 @@ fun commissionAmount(name: String, transferAmount: Int): Int {
             in 300..75_000 -> {
                 transferAmount
             }
-
             else -> {
-                transferAmount - (((transferAmount * 0.6 / 100) + 20).toInt())
+                ((transferAmount * 0.6 / 100) + 20).toInt()
             }
         }
 
         "Mir" -> if (35 < ((transferAmount * 100 / 0.75) - transferAmount)) {
-            return transferAmount - 35
-        } else transferAmount - (transferAmount * 100 / 0.75)
+            return 35
+        } else transferAmount * 100 / 0.75
     }
     return transferAmount
 }
